8daec5ba8a9d150dd30c14f46c6c1330416e632e
Sirpjotr <101092048+Sirpjotr@users.noreply.github.com>
2024-09-05 15:55:22 +0300

    test of mvvm pattern on the task 10.1 completed

---
+++ MVVM/ApplicationViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM
+{
+    internal class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Client selectedClient;
+
+        public ObservableCollection<Client> Clients { get; set; }
+        public Client SelectedClient
+        {
+            get { return selectedClient; }
+            set
+            {
+                selectedClient = value;
+                OnPropertyChanged("SelectedClient");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Clients = new ObservableCollection<Client>
+            {
+                new Client("Попов", "Игорь", "Федорович", "+79453546678", "4017 206555"),
+                new Client("Иванов", "Николай", "Юрьевич", "+79523446768", "4018 543446"),
+                new Client("Морозов", "Михаил", "Александрович", "+79544446709", "4018 347898"),
+                new Client("Смирнов", "Алексей", "Михайлович", "+79346678467", "4019 359878")
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
